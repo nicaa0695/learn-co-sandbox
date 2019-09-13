2a4340e4cac9a9a2d7cd217ac34dd727db29637f
Veronica Lopez <nicaa0695@gmail.com>
2019-09-13 16:36:30 +0000

    modified recipes controller

--- recipes/app/controllers/recipes_controller.rb
+++ recipes/app/controllers/recipes_controller.rb
@@ -19,6 +19,17 @@ class RecipesController < ApplicationController
     end 
   end 
   
+   post '/recipes' do
+    if params[:content] != ""
+      @current_user = User.find(session[:email])
+      @current_user.recipes << Recipe.create(content: params[:content])
+
+       redirect "/recipes"
+    else
+      redirect "/recipes/new"
+    end
+  end
+  
   get '/recipes/:id/edit' do
     if !logged_in?
       redirect "/login" 
@@ -30,6 +41,37 @@ class RecipesController < ApplicationController
     end 
     end 
   end 
+  
+  get '/recipes/:id' do
+    if !logged_in?
+      redirect to "/login"
+    else
+    @recipe = Recipe.find(params[:id])
+
+     erb :"/recipes/show"
+    end
+  end
+  
+   get '/recipes/:id/edit' do
+    if !logged_in?
+      redirect to "/login"
+    else
+      @recipe = Recipe.find(params[:id])
+
+       erb :"/recipes/edit"
+    end
+  end
+  
+  patch '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+    if params[:content] != ""
+      recipe.update(content: params[:content])
+
+       redirect "/recipes/#{recipe.id}"
+    else
+      redirect "/recipes/#{recipe.id}/edit"
+    end
+  end
   
   
 end 

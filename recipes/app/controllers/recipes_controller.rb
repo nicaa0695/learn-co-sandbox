3845bc595f6ac30bb5fd959787c3d536278aa261
Veronica Lopez <nicaa0695@gmail.com>
2019-09-13 21:54:58 +0000

    modified id.rb

--- recipes/app/controllers/recipes_controller.rb
+++ recipes/app/controllers/recipes_controller.rb
@@ -30,18 +30,16 @@ class RecipesController < ApplicationController
     end
   end
   
-  # get '/recipes/:id/edit' do
-  #   if !logged_in?
-  #     redirect "/login" 
-  #   else 
-  #     if recipe = current_user.recipes.find_by(params[:id])
-  #       erb :'recipes/edit'
-  #     # "An edit recipe form. #{current_user.id} is editing #{recipe.id}"
-  #   else 
-  #     redirect '/recipes'
-  #   end 
-  #   end 
-  # end 
+  get '/recipes/id' do 
+    if !logged_in?
+      redirect to "/login"
+    else
+      erb :'/recipes/id'
+      # @recipe.title = params[:title]
+      # @recipe.content = params[:content]
+    end 
+  end 
+      
   
   get '/recipes/:id' do
     if !logged_in?

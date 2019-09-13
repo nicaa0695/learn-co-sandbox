f71a42c7ffa91288b04fdee5205dbd8acefd62e6
Veronica Lopez <nicaa0695@gmail.com>
2019-09-13 17:41:20 +0000

    modified users controller

--- recipes/app/controllers/users_controller.rb
+++ recipes/app/controllers/users_controller.rb
@@ -16,6 +16,12 @@ class UsersController < ApplicationController
       
   end 
   
+   get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+
+       erb :"/users/show"
+    end
+  
   
   
 end 

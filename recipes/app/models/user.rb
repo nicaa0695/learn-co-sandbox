486305c86f24e9af53fd5d894f53fb8f7ca3950a
Veronica Lopez <nicaa0695@gmail.com>
2019-09-13 17:43:16 +0000

    modified user.rb

--- recipes/app/models/user.rb
+++ recipes/app/models/user.rb
@@ -4,5 +4,15 @@ class User < ActiveRecord::Base
   
   has_many :recipes
   
+  def slug
+    slug = self.email.downcase.gsub(' ','-')
+  end
+
+   def self.find_by_slug(slug)
+    User.all.find do |user|
+      user.slug == slug
+    end
+  end
+  
   
 end 

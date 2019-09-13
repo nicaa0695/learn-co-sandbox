0d8c780698ddd97ee4a853e719ef35de2c04e3d3
Veronica Lopez <nicaa0695@gmail.com>
2019-09-13 17:35:36 +0000

    modified index.erb

--- recipes/db/seeds.rb
+++ recipes/db/seeds.rb
@@ -2,15 +2,16 @@ require 'faker'
 include Faker
 
 20.times do 
-  User.create(
+  user = User.create(
     email: Faker::Internet.email,
-    password: Faker::Internet.password(8)
+    password: Faker::Internet.password(min_length: 8)
   )
+  user.save!
 end 
 
 20.times do 
   Recipe.create(
-    title: Faker::Lorem.words(4)
+    title: Faker::Lorem.words(4),
     content: Faker::Lorem.paragraph(2)
   )
 end 
